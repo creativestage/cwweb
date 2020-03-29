2247525c16c33de8363787887caab81efc371815
Xu Jiazheng <jzxu35@best-inc.com>
2020-03-29 13:36:58 +0800

    我的模块增加过滤功能

--- src/pages/ModuleCenter/MyModule/index.vue
+++ src/pages/ModuleCenter/MyModule/index.vue
@@ -2,7 +2,7 @@
 <div class="page-container">
     <div class="search_bar_container">
       <div class="search_keywords" style="width: 400px">
-        <a-input placeholder="模块名/模块描述/模块key值"></a-input>
+        <a-input v-model="keywords" placeholder="模块名/模块描述/模块key值"></a-input>
       </div>
       <a-button class="module-new-btn" type="primary">
         <router-link to="/ModuleEdit">
@@ -11,7 +11,7 @@
       </a-button>
       </div>
     <div class="my-module-list">
-      <ModuleCard  v-for="item in list" :key="item._id" :item="item" />
+      <ModuleCard  v-for="item in filterList" :key="item._id" :item="item" />
     </div>
   </div>
 </template>
@@ -49,9 +49,18 @@
 import ModuleCard from '../../../components/Common/ModuleCard';
 export default {
   data: () => ({
-    msg: 'myModule',
-    list: []
+    list: [],
+    keywords: '',
   }),
+  computed:  {
+    filterList() {
+      const {list, keywords} = this;
+      return list.filter(item => {
+        return item.name && item.name.indexOf(keywords) > -1 || item.desc && item.desc.indexOf(keywords) > -1 ||
+          item.config && item.config.indexOf(keywords) > -1;
+      })
+    }
+  },
   mounted() {
     this.fetchModuleList();
   },
