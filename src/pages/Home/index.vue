d7b9d8caef99b8bb574779e1442a8903a28d7b0f
Xu Jiazheng <jzxu35@best-inc.com>
2020-11-04 12:44:07 +0800

    Fix: 首页增加介绍

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,17 +1,30 @@
 <template>
   <div class="home-container">
-    Welcome
+    <!-- <h2>Welcome</h2> -->
+    <div>
+      <video class="video" controls="controls" autoplay width="700" height="420" src="http://qn.zhangyy.xyz/afull2.mov"></video>
+      <div class="text-center">页面搭建视频教程</div>
+    </div>
   </div>
 </template>
 
-<style>
+<style lang="less">
 .home-container {
   width: 100%;
   height: 100%;
   display:flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   font-size: 48px;
+  .text-center {
+    font-size: 20px;
+    text-align: center;
+  }
+  .video {
+    border: none;
+    outline: none;
+  }
 }
 </style>
 
